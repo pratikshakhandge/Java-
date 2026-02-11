bc7b4440af1631ef2e17ea6dcbfe4554b4479b53
Pratiksha Khandge <pratikshakhandge2006@gmail.com>
2026-02-11 23:55:35 +0530

    Add Geeks class with printMessage method

---
+++ methosInJava.java
@@ -0,0 +1,17 @@
+public class Geeks
+{
+    // An example method
+    public void printMessage() {
+        System.out.println("Hello, Geeks!");
+    }
+    
+    public static void main(String[] args) {
+        
+        // Create an instance of the class
+        // containing the method
+        Geeks obj = new Geeks();
+        
+        // Calling the method
+        obj.printMessage(); 
+    }
+}
